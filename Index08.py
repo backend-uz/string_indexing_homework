030cb70194aae83cbf10f8af6e875ff886edaf50
user1 <backenduz@gmail.com>
2022-11-25 23:01:42 +0500

    done

--- Index08.py
+++ Index08.py
@@ -6,5 +6,8 @@ def main(s):
     Returns:
         int: answer
     """
-    return
-        
+    if s.find('*'):
+        return s.index('*')
+    else:
+        return False
+print(main('ds*jd'))
